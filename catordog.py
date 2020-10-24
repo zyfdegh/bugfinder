ef72373825ef6b06d86e53d01ad2b45055b78173
zyfdegh <zyfdegg@gmail.com>
2020-10-24 12:17:35 +0800

    load test images and predict

--- catordog.py
+++ catordog.py
@@ -17,7 +17,7 @@ train_dir = cur_dir + '/meipian-catdog-data/train'
 train_dogs = 'dog.jpg/*.jpg'
 train_cats = 'cat.jpg/*.jpg'
 
-
+# 训练集
 dataset_dir = tf.keras.utils.get_file(train_dir, origin='')
 dataset_dir = pathlib.Path(dataset_dir)
 
@@ -47,6 +47,7 @@ for image_batch, labels_batch in train_ds:
   print('* label shape: ', labels_batch.shape)
   break
 
+# 验证集
 val_dir = cur_dir + '/meipian-catdog-data/val'
 # val_dogs = 'dog.*.jpg'
 # val_cats = 'cat.*.jpg'
@@ -71,14 +72,30 @@ val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
 num_classes = len(class_names)
 
+# 数据转置，增加样本
+data_augmentation = keras.Sequential(
+  [
+    layers.experimental.preprocessing.RandomFlip("horizontal", 
+                                                 input_shape=(img_height, 
+                                                              img_width,
+                                                              3)),
+    layers.experimental.preprocessing.RandomRotation(0.1),
+    layers.experimental.preprocessing.RandomZoom(0.1),
+  ]
+)
+
+
+# 模型
 model = Sequential([
-  layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+  data_augmentation,
+  layers.experimental.preprocessing.Rescaling(1./255),
   layers.Conv2D(16, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Conv2D(32, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Conv2D(64, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
+  layers.Dropout(0.05),
   layers.Flatten(),
   layers.Dense(128, activation='relu'),
   layers.Dense(num_classes)
@@ -90,9 +107,33 @@ model.compile(optimizer='adam',
 
 model.summary()
 
+# 开始训练
 epochs=10
 history = model.fit(
   train_ds,
   validation_data=val_ds,
   epochs=epochs
 )
+
+# 测试集
+test_dir = cur_dir + '/meipian-catdog-data/test/'
+outputfile = open("output.csv", "w")
+
+## FIXME 写死了 2000 张
+for i in range(2000):
+	img_path = test_dir + str(i) + '.jpg'
+	img = keras.preprocessing.image.load_img(
+    		img_path, target_size=(img_height, img_width)
+	)
+	img_array = keras.preprocessing.image.img_to_array(img)
+	img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+	predictions = model.predict(img_array)
+	score = tf.nn.softmax(predictions[0])
+	
+	class_num = 1 if class_names[np.argmax(score)] == 'dog.jpg' else 0
+	# print("%d,%d,confidence:%.1f" % (i, class_num, 100 * np.max(score)))
+	outputfile.write("%d,%d\n" % (i, class_num))
+
+outputfile.close()
+
